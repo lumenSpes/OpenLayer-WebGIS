87bcecf7c9cb37b0077aadb11389a2a3faf10194
lumenSpes <lumenseps.zee@gmail.com>
2024-08-12 09:20:41 +0600

    vector map implemented

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,68 @@
-window.onload = init();
+window.onload = init;
 
-function init(){
+function init() {
+    const vectorLayer = new ol.layer.Vector({
+        background: "#1a2b39",
+        source: new ol.source.Vector({
+            url: "https://openlayers.org/data/vector/ecoregions.json",
+            format: new ol.format.GeoJSON(),
+        }),
+        style: {
+            "fill-color": ["string", ["get", "COLOR"], "#eee"],
+        },
+    });
+
     const map = new ol.Map({
+        layers: [vectorLayer],
+        target: "map",
         view: new ol.View({
             center: [0, 0],
-            zoom: 7,
+            zoom: 1,
         }),
-        layers: [
-            new ol.layer.Tile({
-                source: new ol.source.OSM()
-            })
-        ],
-        target: 'map'
-    })
+    });
+
+    const featureOverlay = new ol.layer.Vector({
+        source: new ol.source.Vector(),
+        map: map,
+        style: {
+            "stroke-color": "rgba(255, 255, 255, 0.7)",
+            "stroke-width": 2,
+        },
+    });
+
+    let highlight;
+    const displayFeatureInfo = function (pixel) {
+        const feature = map.forEachFeatureAtPixel(pixel, function (feature) {
+            return feature;
+        });
+
+        const info = document.getElementById("info");
+        if (feature) {
+            info.innerHTML = feature.get("ECO_NAME") || "&nbsp;";
+        } else {
+            info.innerHTML = "&nbsp;";
+        }
+
+        if (feature !== highlight) {
+            if (highlight) {
+                featureOverlay.getSource().removeFeature(highlight);
+            }
+            if (feature) {
+                featureOverlay.getSource().addFeature(feature);
+            }
+            highlight = feature;
+        }
+    };
+
+    map.on("pointermove", function (evt) {
+        if (evt.dragging) {
+            return;
+        }
+        const pixel = map.getEventPixel(evt.originalEvent);
+        displayFeatureInfo(pixel);
+    });
+
+    map.on("click", function (evt) {
+        displayFeatureInfo(evt.pixel);
+    });
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,5 @@
 @extends('layout.layout')
 @section('content')
 <div class="map" id="map"></div>
+<div id="info">&nbsp;</div>
 @endsection
